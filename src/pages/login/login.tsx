9b0c94ad51e11d7e82466929b2353b1dd23f95ac
ankit <ankit.sharma21598@gmail.com>
2026-05-10 20:16:38 +0530

    add Login page

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,16 +1,158 @@
+import {
+  Layout,
+  Card,
+  Space,
+  Form,
+  Input,
+  Checkbox,
+  Button,
+  Flex,
+  Alert,
+} from "antd";
+import { LockFilled, UserOutlined, LockOutlined } from "@ant-design/icons";
+import Logo from "../../components/icons/Logo";
+// import { useMutation, useQuery } from "@tanstack/react-query";
+// import { Credentials } from "../../types";
+// import { login, self, logout } from "../../http/api";
+// import { useAuthStore } from "../../store";
+// import { usePermission } from "../../hooks/usePermission";
+
+// const loginUser = async (credentials: Credentials) => {
+//   const { data } = await login(credentials);
+//   return data;
+// };
+
+// const getSelf = async () => {
+//   const { data } = await self();
+//   return data;
+// };
+
 const LoginPage = () => {
+  // const { isAllowed } = usePermission();
+  // const { setUser, logout: logoutFromStore } = useAuthStore();
+
+  // const { refetch } = useQuery({
+  //   queryKey: ["self"],
+  //   queryFn: getSelf,
+  //   enabled: false,
+  // });
+
+  // const { mutate: logoutMutate } = useMutation({
+  //   mutationKey: ["logout"],
+  //   mutationFn: logout,
+  //   onSuccess: async () => {
+  //     logoutFromStore();
+  //     return;
+  //   },
+  // });
+
+  // const { mutate, isPending, isError, error } = useMutation({
+  //   mutationKey: ["login"],
+  //   mutationFn: loginUser,
+  //   onSuccess: async () => {
+  //     const selfDataPromise = await refetch();
+  //     // logout or redirect to client ui
+  //     // window.location.href = "http://clientui/url"
+  //     // "admin", "manager", "customer"
+  //     if (!isAllowed(selfDataPromise.data)) {
+  //       logoutMutate();
+  //       return;
+  //     }
+  //     setUser(selfDataPromise.data);
+  //   },
+  // });
+
   return (
-    <div>
-      <h1>Sign in</h1>
-      <form>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <input type="checkbox" id="remember" />
-        <label htmlFor="remember">Remember me</label>
-        <button type="submit">Login</button>
-        <a href="/forgot-password">Forgot password?</a>
-      </form>
-    </div>
+    <>
+      <Layout
+        style={{ height: "100vh", display: "grid", placeItems: "center" }}
+      >
+        <Space direction="vertical" align="center" size="large">
+          <Layout.Content
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Logo />
+          </Layout.Content>
+          <Card
+            bordered={false}
+            style={{ width: 300 }}
+            title={
+              <Space
+                style={{
+                  width: "100%",
+                  fontSize: 16,
+                  justifyContent: "center",
+                }}
+              >
+                <LockFilled />
+                Sign in
+              </Space>
+            }
+          >
+            <Form
+              initialValues={{
+                remember: true,
+              }}
+              onFinish={(values) => {
+                // mutate({ email: values.username, password: values.password });
+                console.log(values);
+              }}
+            >
+              <Form.Item
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your Username",
+                  },
+                  {
+                    type: "email",
+                    message: "Email is not valid",
+                  },
+                ]}
+              >
+                <Input prefix={<UserOutlined />} placeholder="Username" />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your password",
+                  },
+                ]}
+              >
+                <Input.Password
+                  prefix={<LockOutlined />}
+                  placeholder="Password"
+                />
+              </Form.Item>
+              <Flex justify="space-between">
+                <Form.Item name="remember" valuePropName="checked">
+                  <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+                <a href="" id="login-form-forgot">
+                  Forgot password
+                </a>
+              </Flex>
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  style={{ width: "100%" }}
+                >
+                  Log in
+                </Button>
+              </Form.Item>
+            </Form>
+          </Card>
+        </Space>
+      </Layout>
+    </>
   );
 };
 
